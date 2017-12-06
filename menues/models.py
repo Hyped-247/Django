1bff5299530ed5416e31eceb2acb204afe30e1ef
Mohammad Mahjoub <mmahjoub@westmont.edu>
2017-12-05 21:57:19 -0800

    Making progress

--- menues/models.py
+++ menues/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
+from restaurants.models import RestaurantsLocations
 
-# Create your models here.
+
+class Item(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    restaurant = models.ForeignKey(RestaurantsLocations)
+    name = models.CharField(max_length=120)
+    contents = models.TextField(help_text='Separate each item by comma')
+    excludes = models.TextField(blank=True, null=True, help_text='Separate each item by comma')
+    public = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-updated', '-timestamp']
