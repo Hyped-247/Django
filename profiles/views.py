bf88aa703ffae0a73a8620ac8ff080827fa407c7
Mohammad Mahjoub <mmahjoub@westmont.edu>
2017-12-15 21:34:57 -0800

    Good job. Now just start building websties!

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.http import Http404
 from django.views.generic import DetailView
 from django.shortcuts import get_object_or_404
+from restaurants.models import RestaurantsLocations
 
 User = get_user_model()
 
@@ -15,4 +16,9 @@ class ProfileDetailView(DetailView):
             raise Http404
         return get_object_or_404(User, username__iexact=username, is_active=True)
 
-
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProfileDetailView, self).get_context_data(*args, **kwargs)
+        qs = RestaurantsLocations.objects.filter(owner=self.get_object())
+        if qs.exists():
+            context['locations'] = qs
+        return context
